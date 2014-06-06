fd174522f53a12d33051cc0fb29e0775ac1c4032
khosford <khosford@sapient.com>
2014-06-06 15:18:48 -0400

    try this

--- _js/setup.js
+++ _js/setup.js
@@ -2,7 +2,6 @@
 $(window).load(function(){
 
 	/* #projects */ 
-	setup('#devices');
 	setup('#projects');
 
 	setup('.progress-bar');
@@ -12,25 +11,9 @@ $(window).load(function(){
 function setup(el){
 
 	switch (el) {
-		case ('#devices'):
-			var imgMargin = ($('#devices li').outerHeight() - $('#devices img').outerHeight()) / 2;
-			$('#devices img').css('margin',imgMargin + 'px ' + '0px');
-			break;
         case ('#projects'):
             var height = ( $(window).outerHeight() - ( $('#ribbon').outerHeight() + $('#devices').outerHeight() ) ) + 'px';
             $('#projects').css('height',height);
-
-            var maxApp = 0;
-            $('#apps li').each(function(){
-            	maxApp++;
-            });
-
-            var appWidth = Math.round( ($(window).outerWidth() * 0.22 ));
-            $('#apps li').css('width',appWidth + 'px');
-
-            var width = (appWidth * maxApp + (5 * maxApp));
-            $('#apps').css('width',width + 'px');
-
            	break;
     }
 
