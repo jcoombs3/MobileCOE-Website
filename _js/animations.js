f78a1eacd327f560ff4972623a35e5304249661b
jcoombs3 <jcoombs@sapient.com>
2014-06-06 15:29:26 -0400

    height animo

--- _js/animations.js
+++ _js/animations.js
@@ -2,8 +2,10 @@
 $(window).load(function(){
 
 	$('#projects li').hover(function(e){
-		
+		$(e.currentTarget).find('.app-bar').addClass('hover');
+
 	},function(e){
+		$(e.currentTarget).find('.app-bar').removeClass('hover');
 
 	});
 		
