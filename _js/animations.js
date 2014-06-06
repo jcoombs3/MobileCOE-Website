507274276ae02e143508afd05164379189fdf95c
khosford <khosford@sapient.com>
2014-06-06 16:40:56 -0400

    Clicked state update

--- _js/animations.js
+++ _js/animations.js
@@ -3,16 +3,16 @@ $(window).load(function(){
 
 	$('#projects li').hover(function(e){
 		$(e.currentTarget).find('.app-bar').addClass('hover');
-		$(e.currentTarget).find('.overview').addClass('hover');
+		// $(e.currentTarget).addClass('clicked');
 
 	},function(e){
 		$(e.currentTarget).find('.app-bar').removeClass('hover');
-		$(e.currentTarget).find('.overview').removeClass('hover');
 
 	});
 
-	$('#projects li').on('click', function(){
-		$(e.currentTarget).find('.overview').addClass('clicked');
+	$('#projects li').on('click',function(e){
+		$(e.currentTarget).find('.app-bar').addClass('hover');
+		$(e.currentTarget).addClass('clicked');
 	});
 		
 });
