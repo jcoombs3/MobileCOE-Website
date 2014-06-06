68da788cbc704773e36cacda54b7b444cd0bea9d
khosford <khosford@sapient.com>
2014-06-06 15:58:03 -0400

    Hoverstate

--- _js/animations.js
+++ _js/animations.js
@@ -3,10 +3,16 @@ $(window).load(function(){
 
 	$('#projects li').hover(function(e){
 		$(e.currentTarget).find('.app-bar').addClass('hover');
+		$(e.currentTarget).find('.overview').addClass('hover');
 
 	},function(e){
 		$(e.currentTarget).find('.app-bar').removeClass('hover');
+		$(e.currentTarget).find('.overview').removeClass('hover');
 
+	});
+
+	$('#projects li').on('click', function(){
+		$(e.currentTarget).find('.overview').addClass('clicked');
 	});
 		
 });
